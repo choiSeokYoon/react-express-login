8716cba547e5111ee28b3a48b28eb0b83fbfaf8a
WKWKA <ctd0606@naver.com>
2023-02-23 21:24:25 +0900

    몽고디비 스키마 설정

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const helmet = require("helmet");
-const morgan = require("morgan");
+const mongoose = require("mongoose");//몽고디비의 라이브러리로 데이터를 NODE에서 쉽게 관리하기 위함
+const dotenv = require("dotenv");//환경변수
+const helmet = require("helmet");//express의 보완성을 높여주는 미들웨어
+const morgan = require("morgan");//express의 로그를 기록해줌
 
 dotenv.config();
+mongoose.set("strictQuery", true)
 
-mongoose.connect(
-    process.env.MONGO_URL,
-    {useNewUrlParser: true, useUnifiedTopology: true},
-    ()=>{
-        console.log("MongoDB에 연결됨 ")
+const connect = async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("MongoDB에 연결됨")
+    }catch(err){
+        console.log(err)
     }
-);
+}
+
+
+
+app.listen(8080, ()=>{
+    connect()
+    console.log("서버가 실행중입니다.")
+})
